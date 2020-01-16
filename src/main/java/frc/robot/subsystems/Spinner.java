cbaf7b670cd64d67f3de86a8afcb7737cad23f73
Vis <visfire12@gmail.com>
2020-01-15 22:32:31 -0500

    Removed speed halfer that was used for testing

--- src/main/java/frc/robot/subsystems/Spinner.java
+++ src/main/java/frc/robot/subsystems/Spinner.java
@@ -28,7 +28,7 @@ public class Spinner extends SubsystemBase {
   public Spinner() {
   }
   public void setArmSpeed(double speed){
-    spinnerArm.set((speed/2));
+    spinnerArm.set(speed);
   }
   @Override
   public void periodic() {
